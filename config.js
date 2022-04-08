f044e31f24a5ddab1eb24246f2db24dd07b3efcc
Leonardo <ldagostino@itba.edu.ar>
2022-04-07 22:21:33 -0300

    modules

---
+++ config.js
@@ -0,0 +1,29 @@
+const outFolder = "./out/";
+
+const types = [
+  "body",
+  "div",
+  "button",
+  "class",
+  "h1",
+  "h2",
+  "h3",
+  "h4",
+  "h5",
+  "label",
+  "nav",
+  "svg",
+  "ul",
+  "span",
+  "input",
+  "p",
+  "textarea",
+  "form:input",
+  "form:label",
+  "form:errors",
+  "form:select",
+  "form:option",
+];
+
+exports.outFolder = outFolder;
+exports.types = types;
